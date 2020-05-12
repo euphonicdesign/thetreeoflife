5f939e797f3c2e28134d46cdfbd6b76e624f2b2a
Euphonic Design <25894355+euphonicdesign@users.noreply.github.com>
2020-05-12 19:24:35 +0300

    c27 - factor distantare expandare in v

--- scripts/simulare.js
+++ scripts/simulare.js
@@ -257,9 +257,11 @@ function tufaJetoane(x_start, y_start, nr_straturi){
 
     this.expandare_in_v = function() {
         identare_verticala = 0;
+        factor_distantare_verticala = 2;
         for(let i=0; i<this.nr_straturi; i++){
             var nr_jetoane_per_strat = Math.pow(2,i)
-            identare_verticala += Math.floor(nr_jetoane_per_strat/2)* jeton_model_diametru;
+            identare_verticala += (Math.floor(nr_jetoane_per_strat/2)* jeton_model_diametru * factor_distantare_verticala)/2;
+
             console.log("nr jetoane per strat: " + nr_jetoane_per_strat);
             console.log("nr jetoane per strat/2: " + nr_jetoane_per_strat/2);
 
@@ -272,7 +274,7 @@ function tufaJetoane(x_start, y_start, nr_straturi){
                 else{
                   //nr_jetoane = Math.pow(2,)
                   distantare_orizontala = i * 2 * jeton_model_diametru;
-                  distantare_verticala = j * 1 * jeton_model_diametru;
+                  distantare_verticala = j * factor_distantare_verticala * jeton_model_diametru;
                   y_jeton = this.y_start - identare_verticala + distantare_verticala;
                   myJeton = new jeton(jeton_model_raza, CULOARE_JETON, this.x_start + distantare_orizontala, y_jeton, this.tip_jeton);
 
